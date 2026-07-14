bad25f9801a8b541e000e175a3f7b2b161bd2de9
Jayasudha <jayasudhamudaliyar26@gmail.com>
2026-07-14 14:30:10 +0530

    Enhance ternary operator example program

--- 02-Java-Fundamentals/04-Operators/TernaryOperatorDemo.java
+++ 02-Java-Fundamentals/04-Operators/TernaryOperatorDemo.java
@@ -2,14 +2,37 @@ public class TernaryOperatorDemo {
 
     public static void main(String[] args) {
 
+        System.out.println("===== Ternary Operator =====\n");
+
+        // Example 1: Find Maximum
         int a = 10;
         int b = 20;
 
-        System.out.println("===== Ternary Operator =====\n");
         int max = (a > b) ? a : b;
-        System.out.println("Maximum value : " + max);
-        int age = 18;
-        String result = (age >= 18) ? "Eligible to Vote" : "Not Eligible to Vote";
-        System.out.println(result);
+
+        System.out.println("Maximum Number : " + max);
+
+        System.out.println();
+
+        // Example 2: Even or Odd
+        int number = 5;
+
+        String result = (number % 2 == 0) ? "Even" : "Odd";
+
+        System.out.println(number + " is " + result);
+
+        System.out.println();
+
+        // Example 3: Positive, Negative or Zero
+        int num = 0;
+
+        String value = (num > 0)
+                ? "Positive"
+                : (num < 0)
+                        ? "Negative"
+                        : "Zero";
+
+        System.out.println("Number is " + value);
+
     }
 }
